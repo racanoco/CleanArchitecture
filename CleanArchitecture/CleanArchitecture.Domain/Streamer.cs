3c12e7ad51ad0afa8a23b76562c33e716f99857f
Rcano <Rcanocorbera@gmail.com>
2022-10-07 13:44:05 +0200

    Creacion de modelo

---
+++ CleanArchitecture/CleanArchitecture.Domain/Streamer.cs
@@ -0,0 +1,12 @@
+﻿using System.IO.Pipes;
+
+namespace CleanArchitecture.Domain
+{
+    public class Streamer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;        
+        public string   Url { get; set; } = string.Empty;
+
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Domain/Video.cs
@@ -0,0 +1,16 @@
+﻿namespace CleanArchitecture.Domain
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Clave foranea
+        /// </summary>
+        public int StreamerId { get; set; }
+
+        public virtual Streamer? Streamer { get; set; }
+    }
+}

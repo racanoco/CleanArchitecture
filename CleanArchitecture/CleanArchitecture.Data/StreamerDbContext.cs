3637883c8ccb8b9bf2de2cdbe3dbf25d908a3e11
Rcano <Rcanocorbera@gmail.com>
2022-10-07 14:22:33 +0200

    Cadena de Conexion

---
+++ CleanArchitecture/CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("data source=.\\ANBU;initial catalog=StreamerDB;user id=sa;password=ps015-j;persist security info=True;");
+        }
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+    }
+}

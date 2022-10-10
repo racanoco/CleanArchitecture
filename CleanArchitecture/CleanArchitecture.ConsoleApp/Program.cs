f2058818abe87d89adebd1d06a4706029f1bf432
Rcano <Rcanocorbera@gmail.com>
2022-10-10 17:04:32 +0200

    Consulta Condicionales en EF

--- CleanArchitecture/CleanArchitecture.ConsoleApp/Program.cs
+++ CleanArchitecture/CleanArchitecture.ConsoleApp/Program.cs
@@ -1,12 +1,17 @@
 ﻿using CleanArchitecture.Data;
 using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
 
 StreamerDbContext dbContext = new();
 
 // await AddNewRecords("Disney", "www.disney.com");
 
-QueryStreaming();
+// QueryStreaming();
 
+await QueryFilter();
+
+Console.WriteLine("Presione cualquier tecla para temrinar el proceso");
+Console.ReadKey();
 
 void QueryStreaming()
 {
@@ -18,6 +23,30 @@ void QueryStreaming()
     }
 }
 
+async Task QueryFilter()
+{
+    Console.WriteLine($"Ingrese una compañia de streaming:");
+    
+    string streamerName = Console.ReadLine();
+
+    // var streamers = await dbContext!.Streamers!.Where(x => x.Name == streamerName).ToListAsync();
+    var streamers = await dbContext!.Streamers!.Where(x => x.Name.Equals(streamerName)).ToListAsync();
+
+    foreach (var streamer in streamers)
+    {
+        Console.WriteLine($"{streamer.Id} - {streamer.Name}");
+    }
+
+    // var streamerPartialResultsa = await dbContext!.Streamers!.Where(x => x.Name.Contains(streamerName)).ToListAsync();
+
+    var streamerPartialResultsa = await dbContext!.Streamers!.Where(x => EF.Functions.Like(x.Name, $"%{streamerName}%")).ToListAsync();
+
+    foreach (var streamer in streamerPartialResultsa)
+    {
+        Console.WriteLine($"{streamer.Id} - {streamer.Name}");
+    }
+
+}
 
 async Task AddNewRecords(string streamerName, string streamerUrl)
 {
@@ -68,3 +97,4 @@ async Task AddNewRecords(string streamerName, string streamerUrl)
     }    
 
 }
+

5dfbdf7a80810f66b2f83044ceba9315a957205c
Rcano <Rcanocorbera@gmail.com>
2022-10-20 18:33:38 +0200

    Creacion de interface generica

--- CleanArchitecture/CleanArchitecture.Application/Contracts/Persistence/IAsyncRepository.cs
+++ CleanArchitecture/CleanArchitecture.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -5,11 +5,18 @@ namespace CleanArchitecture.Application.Contracts.Persistence
 {
     public interface IAsyncRepository<T>where T : BaseDomainModel
     {
+        // Retorna una lista de todos los registros de una entidad determinada.
         Task<IReadOnlyList<T>> GetAllAsync();
+
+        // Retorna una colección de datos con una determinada condición lógica dentro de query.
         Task<IReadOnlyList<T>> GetAsync(Expression<Func<T,bool>> expression);
+
+        // Retorna una colección de datos con una determinada condición lógica dentro de query ordenado.
         Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? expression = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
             string? includeString = null,
             bool disableTracking = true);
+
+        // Retorna una colección de datos relacionada entre dentidadescon una determinada condición lógica dentro de query ordenado.
         Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? expression = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
             List<Expression<Func<T, object>>>? includes = null,
             bool disableTracking = true);

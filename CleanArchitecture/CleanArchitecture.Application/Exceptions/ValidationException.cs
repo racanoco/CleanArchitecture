96fe0d4a1a61416555c89d3de7c4588a7ccd9dc4
Rcano <Rcanocorbera@gmail.com>
2022-10-26 15:28:10 +0200

    Excepciones en Validacion

---
+++ CleanArchitecture/CleanArchitecture.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+
+namespace CleanArchitecture.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public ValidationException() : base("Se presentaron uno o mas errores de validacion")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> validationFailures) : this()
+        {
+            Errors = validationFailures
+                .GroupBy(x => x.PropertyName, x => x.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
+        public IDictionary<string, string[]> Errors { get;}
+    }
+}

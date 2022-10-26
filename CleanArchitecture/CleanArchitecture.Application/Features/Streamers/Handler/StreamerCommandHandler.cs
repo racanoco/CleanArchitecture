8884541458c0fc8d60430de27e30c81f551b81e8
Rcano <Rcanocorbera@gmail.com>
2022-10-26 10:43:48 +0200

    Command Handler y CQRS

---
+++ CleanArchitecture/CleanArchitecture.Application/Features/Streamers/Handler/StreamerCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Features.Streamers.Commands;
+using CleanArchitecture.Application.Models;
+using CleanArchitecture.Domain;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Application.Features.Streamers.Handler
+{
+    public class StreamerCommandHandler : IRequestHandler<StreamerCommand, int>
+    {
+        private readonly IStreamerRepository _streamerRepository;
+        private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
+        private readonly ILogger<StreamerCommandHandler> _logger;
+
+        public StreamerCommandHandler(IStreamerRepository streamerRepository, IMapper mapper, IEmailService emailService, ILogger<StreamerCommandHandler> logger)
+        {
+            _streamerRepository = streamerRepository;
+            _mapper = mapper;
+            _emailService = emailService;
+            _logger = logger;
+        }
+
+        public async Task<int> Handle(StreamerCommand request, CancellationToken cancellationToken)
+        {
+            var streamerEntity = _mapper.Map<Streamer>(request);
+
+            var newStreamer = await _streamerRepository.AddAsync(streamerEntity);
+
+            _logger.LogInformation($"Streamer {newStreamer.Id} fue creado exitosamente");
+
+            await SendEmail(newStreamer);
+
+            return newStreamer.Id;
+        }
+
+        private async Task SendEmail(Streamer streamer)
+        {
+            var email = new Email
+            {
+                To = "rcano7@gmail.com",
+                Body = "La compañia de Streamer se creo correctamente",
+                Subject = "Mensaje de alerta"
+            };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception)
+            {
+                _logger.LogError($"Error enviando el email de {streamer.Id}");
+                
+            }
+            
+        }
+    }
+}

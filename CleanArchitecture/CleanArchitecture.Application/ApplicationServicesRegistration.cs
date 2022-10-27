9d10c99fbbe836d0c73878c521e752e74027264d
Rcano <Rcanocorbera@gmail.com>
2022-10-27 10:40:43 +0200

    Crear dependencias en proyecto Application

---
+++ CleanArchitecture/CleanArchitecture.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchitecture.Application.Behaviours;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CleanArchitecture.Application
+{
+    /// <summary>
+    /// Clase estática para la configuración de inyección de dependencias.
+    /// </summary>
+    public static class ApplicationServicesRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}

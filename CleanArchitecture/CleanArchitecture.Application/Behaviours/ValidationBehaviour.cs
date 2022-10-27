8db431955bed8e6d3c13aaaa28c6246c7203a6da
Rcano <Rcanocorbera@gmail.com>
2022-10-27 10:07:26 +0200

    MediatR y Behaviours

---
+++ CleanArchitecture/CleanArchitecture.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using MediatR;
+using ValidationException = CleanArchitecture.Application.Exceptions.ValidationException;
+
+namespace CleanArchitecture.Application.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if(_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var validationResult = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+                var failures = validationResult.SelectMany(r => r.Errors).Where(f => f is not null).ToList();
+
+                if(failures.Count != 0)
+                    throw new ValidationException(failures);
+            }
+
+            return await next();
+        }
+    }
+}

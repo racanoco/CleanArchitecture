a7d82a31dc04557bdd7f942e49e2df6a0746ae7d
Rcano <Rcanocorbera@gmail.com>
2022-11-03 10:25:26 +0100

    Crear metodos controller

---
+++ CleanArchitecture/CleanArchitecture.API/Controllers/StreamerController.cs
@@ -0,0 +1,64 @@
+﻿using CleanArchitecture.Application.Features.Streamers.Commands.CreateStreamer;
+using CleanArchitecture.Application.Features.Streamers.Commands.DeleteStreamer;
+using CleanArchitecture.Application.Features.Streamers.Commands.UpdateStreamer;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace CleanArchitecture.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class StreamerController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StreamerController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        #region POST
+        [HttpPost(Name ="CreateStreamer")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> CreateStreamer([FromBody]CreateStreamerCommand createStreamerCommand)
+        {
+            var response = await _mediator.Send(createStreamerCommand);
+            return Ok(response);
+
+        }
+        #endregion
+
+        #region PUT
+        [HttpPut(Name = "UpdateStreamer")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateStreamer([FromBody] UpdateStreamerCommand updateStreamerCommand)
+        {
+            await _mediator.Send(updateStreamerCommand);
+            
+            return NoContent();
+        }
+        #endregion
+
+        #region DELETE
+        [HttpDelete("{id}", Name = "DeleteStreamer")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> DeleteStreamer(int id)
+        {
+            var deleteStreamerCommand = new DeleteStreamerCommand
+            {
+                Id = id
+            };
+
+            await _mediator.Send(deleteStreamerCommand);
+
+            return NoContent();
+        }
+        #endregion
+    }
+}

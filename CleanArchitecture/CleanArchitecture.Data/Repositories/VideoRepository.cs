464f9f7422820cb694b860301ea69b18c0313053
Rcano <Rcanocorbera@gmail.com>
2022-11-10 03:38:49 +0100

    Implementar metodos personalizados

---
+++ CleanArchitecture/CleanArchitecture.Data/Repositories/VideoRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Domain;
+using CleanArchitecture.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infrastructure.Repositories
+{
+    public class VideoRepository : RepositoryBase<Video>, IVideoRepository
+    {
+        public VideoRepository(StreamerDbContext streamerDbContext) : base(streamerDbContext)
+        {
+        }
+
+        public async Task<Video> GetVideoByName(string videoName)
+        {
+            return await _streamerDbContext.Videos!.Where(x => x.Name == videoName).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Video>> GetVideoByUserName(string userName)
+        {
+            return await _streamerDbContext.Videos!.Where(x => x.CreatedBy == userName).ToListAsync();
+        }
+    }
+}

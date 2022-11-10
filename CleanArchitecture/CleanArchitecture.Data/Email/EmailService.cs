045b9416a7e8aa955e11df616242129c334ec209
Rcano <Rcanocorbera@gmail.com>
2022-11-10 03:52:25 +0100

    Servicios email

---
+++ CleanArchitecture/CleanArchitecture.Data/Email/EmailService.cs
@@ -0,0 +1,47 @@
+﻿
+using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace CleanArchitecture.Infrastructure.Email
+{
+    public class EmailService : IEmailService
+    {
+        public EmailSetting _emailSetting { get; }
+        public ILogger<EmailService> _logger { get; }
+
+        public EmailService(IOptions<EmailSetting> emailSetting, ILogger<EmailService> logger)
+        {
+            _emailSetting = emailSetting.Value;
+            _logger = logger;
+        }
+
+        public async Task<bool> SendEmail(Application.Models.Email email)
+        {
+            var client = new SendGridClient(_emailSetting.ApiKey);
+
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+
+            var from = new EmailAddress
+            {
+                Email = _emailSetting.FromAddress,
+                Name = _emailSetting.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+                return true;
+
+            _logger.LogError("El email no pudo enviarse, existen errores");
+            return false;
+        }
+    }
+}

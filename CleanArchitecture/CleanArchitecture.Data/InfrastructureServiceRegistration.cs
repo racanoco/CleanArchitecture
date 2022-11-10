737b50e5409fd2d866deda535e682a57f0fa93a1
Rcano <Rcanocorbera@gmail.com>
2022-11-10 17:05:01 +0100

    Inyectar dependencias

---
+++ CleanArchitecture/CleanArchitecture.Data/InfrastructureServiceRegistration.cs
@@ -0,0 +1,33 @@
+﻿using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Models;
+using CleanArchitecture.Infrastructure.Email;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure
+{
+    /// <summary>
+    /// Clase estática para la configuración de inyección de dependencias.
+    /// </summary>
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServiceRegistration(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<StreamerDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("connectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IVideoRepository, VideoRepository>();
+            services.AddScoped<IStreamerRepository, StreamerRepository>();
+
+            services.Configure<EmailSetting>(x => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+        
+    }
+}

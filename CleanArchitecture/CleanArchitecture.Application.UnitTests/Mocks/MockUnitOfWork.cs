3faa6845dfb8799fea4e315aec9b2e90de4032a6
Rcano <Rcanocorbera@gmail.com>
2022-11-22 11:43:19 +0100

    Creando Mocks de Unit Of Work

--- CleanArchitecture/CleanArchitecture.Application.UnitTests/Mocks/MockUnitOfWork.cs
+++ CleanArchitecture/CleanArchitecture.Application.UnitTests/Mocks/MockUnitOfWork.cs
@@ -1,6 +1,18 @@
-﻿namespace CleanArchitecture.Application.UnitTests.Mocks
+﻿using CleanArchitecture.Application.Contracts.Persistence;
+using Moq;
+
+namespace CleanArchitecture.Application.UnitTests.Mocks
 {
-    public class MockUnitOfWork
+    public static class MockUnitOfWork
     {
+        public static Mock<IUnitOfWork> GetUnitOfWork() 
+        { 
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            var mockVideoRepository = MockVideoRepository.GetVideoRepository();
+
+            mockUnitOfWork.Setup(r => r.VideoRepository).Returns(mockVideoRepository.Object);
+
+            return mockUnitOfWork;
+        }
     }
 }

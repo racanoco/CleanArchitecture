33db413691028e19e4b4e6ecf9a0c32e26bda222
Rcano <Rcanocorbera@gmail.com>
2022-11-23 10:36:05 +0100

    Base de datos InMemory EF y UnitTest

--- CleanArchitecture/CleanArchitecture.Application.UnitTests/Features/Video/Queries/GetVideosListQueryHandlerXUnitTests.cs
+++ CleanArchitecture/CleanArchitecture.Application.UnitTests/Features/Video/Queries/GetVideosListQueryHandlerXUnitTests.cs
@@ -1,6 +1,44 @@
-﻿namespace CleanArchitecture.Application.UnitTests.Features.Video.Queries
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Features.Videos.Handler;
+using CleanArchitecture.Application.Features.Videos.Queries;
+using CleanArchitecture.Application.Features.Videos.ViewModels;
+using CleanArchitecture.Application.Mappings;
+using CleanArchitecture.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace CleanArchitecture.Application.UnitTests.Features.Video.Queries
 {
     public class GetVideosListQueryHandlerXUnitTests
     {
+        private readonly IMapper _mapper;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+
+        public GetVideosListQueryHandlerXUnitTests()
+        {
+            _unitOfWork = MockUnitOfWork.GetUnitOfWork();
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<VideoProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetVideosListTest()
+        {
+            string userName = "racanoco";
+            var handler = new GetVideosListQueryHandler(_unitOfWork.Object, _mapper);
+            var request = new GetVideosListQuery(userName);
+
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            result.ShouldBeOfType<List<VideoViewModel>>();
+
+            result.Count.ShouldBe(1);
+        }
     }
 }
--- CleanArchitecture/CleanArchitecture.Application.UnitTests/Mocks/MockVideoRepository.cs
+++ CleanArchitecture/CleanArchitecture.Application.UnitTests/Mocks/MockVideoRepository.cs
@@ -1,20 +1,40 @@
 ﻿using AutoFixture;
-using CleanArchitecture.Application.Contracts.Persistence;
 using CleanArchitecture.Domain;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace CleanArchitecture.Application.UnitTests.Mocks
 {
     public static class MockVideoRepository
     {
-        public static Mock<IVideoRepository> GetVideoRepository()
+        public static Mock<VideoRepository> GetVideoRepository()
         {
             var fixture = new Fixture();
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior()); // Evitar error "AutoFixture.ObjectCreationExceptionWithPath" al ejecutar un método de prueba.
             var videos = fixture.CreateMany<Video>().ToList();
 
-            var mockRepository = new Mock<IVideoRepository>();
+            // Pasar a una funcion privada.
+            // INICIO---Configuración de la base de datos en memoria utilizamos el paquete EntityFrameworkCore.InMemory.---------------------------------------------
+            // Agrega un nuevo registro de prueba creado por el UserName "racanoco" en la entidad Video.
+            videos.Add(fixture.Build<Video>()
+                .With(tr => tr.CreatedBy, "racanoco")
+                .Create()
+                );
 
-            mockRepository.Setup(r => r.GetAllAsync()).ReturnsAsync(videos);
+            // Configuración de la base de datos en memoria, 
+            var options = new DbContextOptionsBuilder<StreamerDbContext>()
+                .UseInMemoryDatabase(databaseName: $"NewInstanceStreamerDbContextFake-{Guid.NewGuid()}")
+                .Options;
+
+            // Creando la instancia del entity framework
+            var streamerDbContextFake = new StreamerDbContext(options);
+            streamerDbContextFake.Videos!.AddRange(videos);
+            streamerDbContextFake.SaveChanges();
+            // FIN-------------------------------------------------------------------------------------------------
+
+            var mockRepository = new Mock<VideoRepository>(streamerDbContextFake);          
 
             return mockRepository;
         }
